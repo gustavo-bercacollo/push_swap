01bcac6263b4e111125182a8111ee662b6313e39
gustavo <gustavovilela802@gmail.com>
2025-09-30 16:42:49 -0300

    add rr

--- src/rotate.c
+++ src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: gbercaco <gbercaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 15:01:21 by gbercaco          #+#    #+#             */
-/*   Updated: 2025/09/30 15:40:36 by gbercaco         ###   ########.fr       */
+/*   Updated: 2025/09/30 16:38:17 by gbercaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void ra(t_piles *piles)
   last = last->next;
   
  last->next = first;
- last->next->next = NULL;
+ first->next = NULL;
 }
 
 void rb(t_piles *piles)
@@ -49,5 +49,34 @@ void rb(t_piles *piles)
   last = last->next;
   
  last->next = first;
- last->next->next = NULL;
+ first->next = NULL;
 }
+
+void rr(t_piles *piles)
+{
+    t_stack *first;
+    t_stack *last;
+
+    if (piles->pile_a && piles->pile_a->next)
+    {
+        first = piles->pile_a;
+        piles->pile_a = first->next;
+        last = piles->pile_a;
+        while (last->next)
+            last = last->next;
+        last->next = first;
+        first->next = NULL;
+    }
+
+    if (piles->pile_b && piles->pile_b->next)
+    {
+        first = piles->pile_b;
+        piles->pile_b = first->next;
+        last = piles->pile_b;
+        while (last->next)
+            last = last->next;
+        last->next = first;
+        first->next = NULL;
+    }
+}
+
